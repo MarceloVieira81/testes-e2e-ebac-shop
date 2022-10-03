f969903ce4a42306011d6d5eeebdfc324d197579
Marcelo Vieira <mv.marcelovieira.0@gmail.com>
2022-10-03 20:44:28 -0300

    Atualização cadastro usando faker no lugar do login

--- cypress/e2e/e2e.cy.js
+++ cypress/e2e/e2e.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
-let dadosLogin
-import perfil from '../fixtures/perfil.json';
+
 import EnderecoPage from '../support/page_objects/endereco.page'
+const { faker } = require('@faker-js/faker');
 
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
@@ -14,18 +14,22 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         E validando minha compra ao final */
 
     beforeEach(() => {
-        cy.visit('minha-conta') 
+        cy.visit('minha-conta')
     });
-        
-    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        cy.fixture('perfil').then(dados => {
-            cy.get('#username').type(dados[1].usuario)
-            cy.get('#password').type(dados[1].senha)
-            cy.get('.woocommerce-form > .button').click()
-            cy.get('.page-title').should('contain', 'Minha conta')
-        })
-        
-       
+
+    it('testando faker', () => {
+        let nomeFaker = faker.name.firstName()
+        let sobrenomeFaker = faker.name.lastName()
+        let emailFaker = faker.internet.email(nomeFaker)
+
+        cy.get('#reg_email').type(emailFaker)
+        cy.get('.register > :nth-child(2) > label').type('!teste@testes')
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(nomeFaker)
+        cy.get('#account_last_name').type(sobrenomeFaker)
+        cy.get('.woocommerce-Button').click()      
+    
         cy.get('.woocommerce-MyAccount-navigation-link--edit-address > a').click()
         cy.get(':nth-child(1) > .title > .edit').click()
 
@@ -47,15 +51,15 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         cy.get('.dropdown-toggle > .text-skin').click()
         cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click()
         cy.get('.woocommerce-billing-fields > h3').should('contain', 'Detalhes de faturamento')
-        
+
         cy.get('#terms').click()
-        cy.get('#place_order').click({force: true})
+        cy.get('#place_order').click({ force: true })
         cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
 
 
     });
 
-    
+
 
 
 })
